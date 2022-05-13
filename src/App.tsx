2228be4411ebe5d9cc4afdcaccd23f29902e30b2
David Woolsey <woolsey2316@gmail.com>
2022-05-13 20:16:57 +1000

    expand table info

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import { useAppDispatch, useAppSelector } from './app/hooks'
-import { incremented, amountAdded } from './features/counter/counter-slice'
 import { useFetchBreedsQuery } from './features/dogs/dogs-slice-api'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
@@ -12,35 +10,32 @@ function App() {
   const [numDogs, setNumbDogs] = useState(10)
   const { data = [], isFetching } = useFetchBreedsQuery(numDogs)
 
-  function handleClick () {
-    dispatch(amountAdded(3))
-  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={handleClick}>
-            count is: {count}
-          </button>
-        </p>
         <div>
           <p>dogs to fetch:</p>
-          <select value={numDogs} onChange={(e) => setNumbDogs(Number(e.target.value))}>
-            <option value={"5"}></option>
-            <option value={"10"}></option>
-            <option value={"15"}></option>
-            <option value={"20"}></option>
+          <select onChange={(e) => setNumbDogs(Number(e.target.value))}>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="100">100</option>
           </select>
         </div>
         <div>
-          <p>number of dogs fetched: {data.length} </p>
+          <p>Number of dogs fetched: {data.length} </p>
+          { isFetching && <div>loading dogs...</div>}
+          { !isFetching &&
           <table>
             <thead>
               <tr>
                 <th>Name</th>
+                <th>Breed Group</th>
                 <th>Picture</th>
+                <th>Bred for</th>
+                <th>Weight</th>
+                <th>Origin</th>
+                <th>Life Span</th>
               </tr>
             </thead>
             <tbody>
@@ -48,34 +43,21 @@ function App() {
                 data.map((breed) => (
                   <tr key={breed.id}>
                     <td>{breed.name}</td>
+                    <td>{breed.breed_group}</td>
                     <td>
                       <img src={breed.image.url} alt={breed.name} height={250}></img>
                     </td>
+                    <td>{breed.bred_for}</td>
+                    <td>{breed.weight.metric}kg</td>
+                    <td>{breed.origin}</td>
+                    <td>{breed.life_span}</td>
                   </tr>
                 ))
               }
             </tbody>
           </table>
+          }
         </div>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </header>
     </div>
   )
--- src/features/dogs/dogs-slice-api.ts
+++ src/features/dogs/dogs-slice-api.ts
@@ -3,6 +3,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const DOGS_API_KEY = '892a5907-8d8a-4fc5-80f8-1496b219fb3e'
 
 interface Breed {
+    life_span: string;
+    origin: string;
+    breed_group: string;
+    bred_for: string;
+    weight: {imperial: string, metric: string};
     id: string;
     name: string;
     image: {
